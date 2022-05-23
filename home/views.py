92cd93476c958a517c471c3df0cbe0a2a12d2e51
Mathenge-Alex <arxmaina@gmail.com>
2022-05-24 02:22:17 +0300

    Feat: Make superuser as admin and create urls.py file to separate app home

---
+++ home/views.py
@@ -0,0 +1,12 @@
+import django
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
