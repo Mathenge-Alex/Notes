608c2bbdbc6723799d2a6699acebd6fef50397fd
Mathenge-Alex <arxmaina@gmail.com>
2022-05-24 20:45:39 +0300

    Feat: CRUD (Update): Add back, edit, and cancel-edit functionalities.

--- notes/views.py
+++ notes/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from .models import Notes
 from .forms import NotesForm
 
+# Creating View
 class NotesCreateView(CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
 
+# Listing View
 class NotesListView(ListView):
     model = Notes
     context_object_name = 'notes'
     template_name: str = 'notes/notes_list.html'  # Can include it or not if the standard naming is followed
 
-
-# def list(request):
-#     all_notes = Notes.objects.all()
-#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+# Updating View
+class NotesUpdateView(UpdateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
 
 
 class NotesDetailView(DetailView):
@@ -26,13 +29,3 @@ class NotesDetailView(DetailView):
     context_object_name = 'note'
     # template_name: str = 'notes/notes_detail.html'
     #  # No template name . . .. standard naming saved us
-
-
-# def detail(request, pk):
-#     try:
-#         note = Notes.objects.get(pk=pk)
-#     except Notes.DoesNotExist:
-#         raise Http404("Note doesn't exist")
-#     return render(request, 'notes/notes_detail.html', {'note': note})
-
-
