855326441935f7c24584e42ddcb250f27f558ba3
Mathenge-Alex <arxmaina@gmail.com>
2022-05-24 11:23:12 +0300

    Refact: Change Home page view to a class-based view with HomeView inheriting from TemplateView

--- home/views.py
+++ home/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
+# def home(request):
+#     return render(request, 'home/welcome.html', {'today': datetime.today()})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
+
+
 @login_required(login_url='/admin')
 def authorized(request):
     return render(request, 'home/authorized.html', {})
