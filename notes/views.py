9571482b1f3a097c2ee1292a305280ea6e75df2c
Mathenge-Alex <arxmaina@gmail.com>
2022-05-24 11:51:54 +0300

    Refact: Replace list and detail functions with ListView and DetailView for more robust code.

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import Http404
-# Create your views here.
-
+from django.views.generic import ListView, DetailView
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name: str = 'notes/notes_list.html'  # Can include it or not if the standard naming is followed
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    # template_name: str = 'notes/notes_detail.html'
+    #  # No template name . . .. standard naming saved us
+
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
+
+
