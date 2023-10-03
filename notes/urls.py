9f40fdc6213d9ea67e74c8060d3da8fba725d738
Mathenge-Alex <arxmaina@gmail.com>
2023-10-03 04:34:45 +0300

    Add a general page for all notes

--- notes/urls.py
+++ notes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),
     path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name="notes.delete"),
     path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    path('notes/conf', views.Cofused, name="confused"),
 ]
